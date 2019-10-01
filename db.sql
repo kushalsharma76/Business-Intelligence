d8495f3bb44c128b8f0fd56a55befd4b74d1e4cb
kushalsharma76 <kushalsharma76@gmail.com>
2019-10-01 17:07:11 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,8 @@
+-- Create a Database
+create database riims;
+-- use a database
+use riims
+-- create table
+create table user (id int(10), name varchar(40));
+-- Insert records
+insert into user values (100, "Ravi");
